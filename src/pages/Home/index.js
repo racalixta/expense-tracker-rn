6354cba3a357bb1d49d32b1489189320068100a6
racalixta <ra.calixta@gmail.com>
2023-12-25 17:01:48 -0300

    Atualizacao da Home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,12 +1,48 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
 import Header from '../../components/Header';
+import Balance from '../../components/Balance';
+import Movement from '../../components/Movements';
+
+const list = [
+  {
+    id: 1,
+    label: "Salário",
+    value: "6500,00",
+    date: "03/12/2023",
+    type: 1 // balance
+  },
+  {
+    id: 2,
+    label: "Boleto conta luz",
+    value: "300,00",
+    date: "05/12/2023",
+    type: 0 // expenses
+  },
+  {
+    id: 3,
+    label: "Pix Cliente A",
+    value: "230,00",
+    date: "21/12/2023",
+    type: 1 // balance
+  },
+]
 
 export default function Home() {
   return (
     <View style={styles.container}>
       <Header name="John Dude" />
 
-      <Text>Home</Text>
+      <Balance balance="1.000,00" expense="300,00" />
+
+      <Text style={styles.title}>Últimas Movimentações</Text>
+
+      <FlatList 
+        style={styles.list}
+        data={list}
+        keyExtractor={ (item) => String(item.id) }
+        showsVerticalScrollIndicator={false}
+        renderItem={ ( {item} ) => <Movement data={item} />}
+      />
 
     </View>
   );
@@ -15,6 +51,14 @@ export default function Home() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#fafafa',
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: "bold",
+    margin: 14,
+  },
+  list: {
+    marginHorizontal: 14,
   },
 });
